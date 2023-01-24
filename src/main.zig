1dca18c98ea3edd27b73ac428d356f8137e5f185
glyh <lyhokia@gmail.com>
2023-01-24 10:50:42 -0800

    Add + and - operators

--- src/main.zig
+++ src/main.zig
@@ -2,25 +2,16 @@ const std = @import("std");
 const clap = @import("clap");
 
 const stdout = std.io.getStdOut().writer();
-pub fn run(a: std.mem.Allocator, code: []u8) !u32 {
+pub fn run(a: std.mem.Allocator, code: []const u8) !u32 {
     var tmp_d = std.testing.tmpDir(.{});
     defer tmp_d.cleanup();
 
-    try stdout.print(
-        \\Compiled code:
-        \\-------------
-        \\{s}
-        \\-------------
-    , .{code});
-
-    try stdout.print("Writing compiled code to {}", .{tmp_d.dir});
     try tmp_d.dir.writeFile("tmp.s", code);
-    var link_result = try std.ChildProcess.exec(.{
+    _ = try std.ChildProcess.exec(.{
         .allocator = a,
         .argv = &[_][]const u8{ "gcc", "-static", "-o", "tmp", "tmp.s" },
         .cwd_dir = tmp_d.dir,
     });
-    try stdout.print("Link result: {s}\n{s}\n", .{ link_result.stdout, link_result.stderr });
     var run_result = try std.ChildProcess.exec(.{
         .allocator = a,
         .argv = &[_][]const u8{"./tmp"},
@@ -36,25 +27,73 @@ pub fn run(a: std.mem.Allocator, code: []u8) !u32 {
 }
 
 // returns file path
-pub fn compile(a: std.mem.Allocator, n: i32) ![]u8 {
-    return std.fmt.allocPrint(a,
+pub fn compile(a: std.mem.Allocator, src: []const u8) ![]u8 {
+    var generated_asm = std.ArrayList(u8).init(a);
+    const writer = generated_asm.writer();
+
+    try writer.writeAll(
         \\  .global main
         \\main:
-        \\  mov ${}, %rax
-        \\  ret
         \\
-    , .{n});
+    );
+    var cur_int: i32 = 0;
+    // var last_is_plus: ?bool = null;
+    const Operator = enum { mov, add, sub };
+    var last_op = Operator.mov;
+
+    for (src) |char| {
+        switch (char) {
+            '0'...'9' => {
+                cur_int = cur_int * 10 + char - '0';
+            },
+            '+', '-' => {
+                switch (last_op) {
+                    Operator.mov => {
+                        try writer.print("  mov ${}, %rax\n", .{cur_int});
+                    },
+                    Operator.add => {
+                        try writer.print("  add ${}, %rax\n", .{cur_int});
+                    },
+                    Operator.sub => {
+                        try writer.print("  sub ${}, %rax\n", .{cur_int});
+                    },
+                }
+                if (char == '+') { // add
+                    last_op = Operator.add;
+                } else { // sub
+                    last_op = Operator.sub;
+                }
+                cur_int = 0;
+            },
+            else => continue,
+        }
+    }
+    // TODO: read expression supporting +, -
+    switch (last_op) {
+        Operator.mov => {
+            try writer.print("  mov ${}, %rax\n", .{cur_int});
+        },
+        Operator.add => {
+            try writer.print("  add ${}, %rax\n", .{cur_int});
+        },
+        Operator.sub => {
+            try writer.print("  sub ${}, %rax\n", .{cur_int});
+        },
+    }
+    try writer.writeAll("  ret\n");
+    const out = generated_asm.toOwnedSlice();
+    return out;
 }
 
-pub fn compile_and_run(a: std.mem.Allocator, n: i32) !u32 {
-    const code = try compile(a, n);
-    defer a.free(code);
-    return run(a, code);
+pub fn compile_and_run(a: std.mem.Allocator, src: []const u8) !u32 {
+    const generated_asm = try compile(a, src);
+    defer a.free(generated_asm);
+    return run(a, generated_asm);
 }
 
 pub fn main() !void {
     const params = comptime clap.parseParamsComptime(
-        \\-n, --number <i32>   An option param
+        \\-s, --source <str>   Source code to compile
     );
 
     var diag = clap.Diagnostic{};
@@ -69,17 +108,22 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(!gpa.deinit());
     const a = gpa.allocator();
-    if (res.args.number) |n| {
-        const compile_out = try compile(a, n);
+    if (res.args.source) |s| {
+        const compile_out = try compile(a, s);
         defer a.free(compile_out);
-
         try stdout.writeAll(compile_out);
     }
 }
 
 test "one integer" {
     const testing = std.testing;
-    try testing.expectEqual(try compile_and_run(testing.allocator, 42), 42);
-    try testing.expectEqual(try compile_and_run(testing.allocator, 1), 1);
-    try testing.expectEqual(try compile_and_run(testing.allocator, 0), 0);
+    try testing.expectEqual(try compile_and_run(testing.allocator, &"42".*), 42);
+    try testing.expectEqual(try compile_and_run(testing.allocator, &"1".*), 1);
+    try testing.expectEqual(try compile_and_run(testing.allocator, &"0".*), 0);
+}
+
+test "add/sub" {
+    const testing = std.testing;
+    try testing.expectEqual(try compile_and_run(testing.allocator, &"1+2+3".*), 6);
+    try testing.expectEqual(try compile_and_run(testing.allocator, &"5+20-4".*), 21);
 }
